43037093bce1d5bbdb75ac394284432899f7d736
Dwi Satrio W. <31796624+dwisatriow@users.noreply.github.com>
2025-05-20 19:03:52 +0700

    learn(gql): using mutations in React

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { SigninMutation } from '@/gql/signinMutation'
 import { setToken } from '@/utils/token'
 import { Button, Input } from '@nextui-org/react'
 import { useRouter } from 'next/navigation'
@@ -9,9 +10,16 @@ import { useMutation } from 'urql'
 const SigninPage = () => {
   const [state, setState] = useState({ password: '', email: '' })
   const router = useRouter()
+  const [signinResult, signin] = useMutation(SigninMutation)
 
   const handleSignin = async (e) => {
     e.preventDefault()
+    const result = await signin({ input: state })
+
+    if (result.data.signin) {
+      setToken(result.data.signin.token)
+      router.push('/')
+    }
   }
 
   return (
--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,15 +1,25 @@
 'use client'
 
+import { SignupMutation } from '@/gql/signupMutation'
+import { setToken } from '@/utils/token'
 import { Button, Input } from '@nextui-org/react'
 import { useRouter } from 'next/navigation'
 import { useState } from 'react'
+import { useMutation } from 'urql'
 
 const SignupPage = () => {
   const [state, setState] = useState({ password: '', email: '' })
   const router = useRouter()
+  const [signupResult, signup] = useMutation(SignupMutation)
 
   const handleSignup = async (e) => {
     e.preventDefault()
+    const result = await signup({ input: state })
+
+    if (result.data.createUser) {
+      setToken(result.data.createUser.token)
+      router.push('/')
+    }
   }
 
   return (

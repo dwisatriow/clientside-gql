cc5a82bf29d97411e28304696e3f48dd27a087e1
Dwi Satrio W. <31796624+dwisatriow@users.noreply.github.com>
2025-05-26 21:43:29 +0700

    learn(queries): wiring queries to the UI

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import PageHeader from '../_components/PageHeader'
-import { useMutation, useQuery } from 'urql'
+import { useMutation, useQuery } from '@urql/next'
 import { useState } from 'react'
 import {
   Button,
@@ -18,6 +18,7 @@ import {
 import { PlusIcon } from 'lucide-react'
 import Issue from '../_components/Issue'
 import { IssuesQuery } from '@/gql/issuesQuery'
+import { CreateIssueMutation } from '@/gql/createIssueMutation'
 
 const IssuesPage = () => {
   const { isOpen, onOpen, onOpenChange } = useDisclosure()
@@ -25,7 +26,25 @@ const IssuesPage = () => {
   const [issueDescription, setIssueDescription] = useState('')
   const [{ data, error, fetching }, replay] = useQuery({ query: IssuesQuery })
 
-  const onCreate = async (close) => {}
+  const [createIssueResult, createIssue] = useMutation(CreateIssueMutation)
+
+  const onCreate = async (close) => {
+    const result = await createIssue({
+      input: {
+        name: issueName,
+        content: issueDescription,
+      },
+    })
+
+    if (result.error) {
+      console.error(result.error)
+    }
+    if (result.data) {
+      close()
+      setIssueName('')
+      setIssueDescription('')
+    }
+  }
 
   return (
     <div>
